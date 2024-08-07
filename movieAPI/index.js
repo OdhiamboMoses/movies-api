01865d9bde8e9109cabd94cddc5b2bbaeb7dfad0
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-08-07 16:00:55 +0300

    input validation

--- movieAPI/index.js
+++ movieAPI/index.js
@@ -4,57 +4,11 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const display = document.getElementById("movie-container");
 const inputText = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
+const err = document.getElementById("err");
 
 searchBtn.addEventListener("click", function () {
   let searchValue = inputText.value;
-  display.innerHTML = "";
-  const request = fetch(
-    `${BASE_URL}/search/movie?query=${searchValue}&api_key=${API_KEY}`
-  );
-  request
-    .then((res) => {
-      return res.json();
-    })
-    .then((movies) => {
-      console.log(movies);
-      for (let movie of movies.results) {
-        const title = movie.title;
-        const reviews = movie.vote_average;
-        const releaseDate = movie.release_date;
-        const card = document.createElement("div");
-        card.classList.add("card");
-        display.appendChild(card);
-
-        const heading = document.createElement("h2");
-        heading.textContent = title;
-        card.appendChild(heading);
-
-        const image = document.createElement("img");
-        image.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-
-        card.appendChild(image);
-
-        const paragraph = document.createElement("p");
-        paragraph.textContent = `Release date: ${releaseDate}:${reviews}`;
-
-        card.appendChild(paragraph);
-        console.log(
-          `${title} (${releaseDate.slice(0, 4)}) has ${Math.floor(
-            reviews
-          )}/10 stars.`
-        );
-      }
-    })
-    .catch((err) => {
-      console.dir(err);
-    });
-
-  inputText.value = "";
-});
-
-inputText.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    let searchValue = inputText.value;
+  if (searchValue !== "") {
     display.innerHTML = "";
     const request = fetch(
       `${BASE_URL}/search/movie?query=${searchValue}&api_key=${API_KEY}`
@@ -83,7 +37,7 @@ inputText.addEventListener("keypress", function (e) {
           card.appendChild(image);
 
           const paragraph = document.createElement("p");
-          paragraph.textContent = `Release date: ${releaseDate}:${reviews}`;
+          paragraph.textContent = `Release date: ${releaseDate}: =>${reviews}`;
 
           card.appendChild(paragraph);
           console.log(
@@ -98,5 +52,60 @@ inputText.addEventListener("keypress", function (e) {
       });
 
     inputText.value = "";
+  } else {
+    err.textContent = "Input required.";
+  }
+});
+
+inputText.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    let searchValue = inputText.value;
+    if (searchValue !== "") {
+      display.innerHTML = "";
+      const request = fetch(
+        `${BASE_URL}/search/movie?query=${searchValue}&api_key=${API_KEY}`
+      );
+      request
+        .then((res) => {
+          return res.json();
+        })
+        .then((movies) => {
+          console.log(movies);
+          for (let movie of movies.results) {
+            const title = movie.title;
+            const reviews = movie.vote_average;
+            const releaseDate = movie.release_date;
+            const card = document.createElement("div");
+            card.classList.add("card");
+            display.appendChild(card);
+
+            const heading = document.createElement("h2");
+            heading.textContent = title;
+            card.appendChild(heading);
+
+            const image = document.createElement("img");
+            image.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+
+            card.appendChild(image);
+
+            const paragraph = document.createElement("p");
+            paragraph.textContent = `Release date: ${releaseDate}:${reviews}`;
+
+            card.appendChild(paragraph);
+            console.log(
+              `${title} (${releaseDate.slice(0, 4)}) has ${Math.floor(
+                reviews
+              )}/10 stars.`
+            );
+          }
+        })
+        .catch((err) => {
+          console.dir(err);
+        });
+
+      inputText.value = "";
+    } else {
+      err.textContent = "Input required.";
+    }
   }
 });
